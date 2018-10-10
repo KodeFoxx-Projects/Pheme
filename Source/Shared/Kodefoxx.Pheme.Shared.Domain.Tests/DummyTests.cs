42a85d4a6df429b7b0b49d7a152da9e63bd23686
Yves Schelpe <yves.schelpe@gmail.com>
2018-10-10 19:27:16 +0200

    Shared "Domain" & "Infrastucture" projects. #2 Test projects #3

---
+++ Source/Shared/Kodefoxx.Pheme.Shared.Domain.Tests/DummyTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Kodefoxx.Pheme.Shared.Domain.Tests
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void Is_always_true()
+            => Assert.True(true)
+        ;
+    }
+}
